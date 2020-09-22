c26461524dfb64aafb417a10089dbc86301b832a
vagrant <vagrant@homestead>
2020-09-22 02:24:53 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+// 个人页面相关路由
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //Route::get('/home', 'HomeController@index')->name('home');
