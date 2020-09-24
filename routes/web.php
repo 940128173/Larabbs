48d21e428f7c4bf82f82b78cf877896ebe4e5849
vagrant <vagrant@homestead>
2020-09-24 03:41:18 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 // 个人页面相关路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
