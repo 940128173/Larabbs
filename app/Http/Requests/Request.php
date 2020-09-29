cfeb2ceed84f08f00505080195064bce428f0dcd
vagrant <vagrant@homestead>
2020-09-29 03:03:25 +0000

    用户可以创建话题

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -12,3 +12,39 @@ class Request extends FormRequest
         return true;
     }
 }
+
+class TopicRequest extends Request
+{
+    piblic function rules()
+    {
+        switch($this->method())
+        {
+            // CREATE
+            case 'POST':
+            // UPDATE
+            case 'PUT':
+            case 'PATCH':
+            {
+                return [
+                    'title' => 'required|min:2',
+                    'body' =>'required|min:3',
+                    'category_id' => 'required|numeric',
+                ];
+            }
+            case 'GET':
+            case 'DELETE':
+            default:
+            {
+                return [];
+            }
+        }
+    }
+
+    public function messages()
+    {
+        return [
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
+        ];
+    }
+}
