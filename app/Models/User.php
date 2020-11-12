82ccb8ab25527b500b2331625e65976d25e8f3f7
vagrant <vagrant@homestead>
2020-11-12 08:06:21 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    public function relies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
