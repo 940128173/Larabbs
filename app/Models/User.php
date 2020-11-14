127f0dd94c426ca4422cd6cc2f1328903967f80b
vagrant <vagrant@homestead>
2020-11-14 09:20:38 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
     use Notifiable {
         notify as protected laravelNotify;
     }
+
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
@@ -22,7 +23,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
             return;
         }
 
-        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都Pass
         if (method_exists($instance, 'toDatabase')) {
             $this->increment('notification_count');
         }
@@ -70,5 +71,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
